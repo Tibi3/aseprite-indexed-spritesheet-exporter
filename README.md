d273ffa168dead48400e1540e08ed781c9528cb4
Tibi <gubonyitibor@gmail.com>
2026-02-23 17:19:28 +0100

    add script, add readme

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# aseprite-indexed-spritesheet-exporter
+# Aseprite Indexed Spritesheet Exporter
+
+This script exports a sprite as a spritesheet and replaces the colors with the color indeces.  
+Useful for implementing palette swapping in games.
+
+## Installing
+
+- Open Aseprite
+- File -> Scripts -> Open Scripts Folder
+- Copy export-indexed-spritesheet.lua there.
+- File -> Scripts -> Rescan Scripts Folder
+
+## How to use
+
+- Open a sprite
+- File -> Scripts -> export-indexed-spritesheet
+- Choose where to save the spritesheet
+- Click confirm
+- Set options for the spritesheet but DO NOT modify the output file path
+- Click export
+
+
+If you don't want to use sprietsheets check out [aseprite-index-exporter](https://github.com/mikeemm/aseprite-index-exporter/tree/master).
---
+++ export-indexed-spritesheet.lua
@@ -0,0 +1,95 @@
+local TMP_FILE_PATH <const> = app.fs.joinPath(app.fs.tempPath, "indexed_sprite_sheet.png")
+
+function main()
+    if app.isUIAvailable == false then
+        print("Headless mode not supported.")
+        return
+    end
+
+    if app.sprite == nil then
+        app.alert("Please open a sprite you want to export.")
+        return
+    end
+
+    if app.sprite.colorMode ~= ColorMode.INDEXED then
+        app.alert("Please change color mode to indexed.")
+        return
+    end
+
+    local file_path = openFileDialog()
+    if file_path == nil then
+        return
+    end
+
+    if openExportSpritesheetDialog() == false then
+        return
+    end
+
+    create_indexed_image(file_path)
+end
+
+function openFileDialog()
+    local stop_script = true
+
+    local dlg = Dialog {
+        title = "Export image as spritesheet"
+    }
+
+    local file_path = dlg:label {
+        text = "Do NOT change the output file in the next dialog!!"
+    }:file {
+        id = "file_path",
+        label = "Output File:",
+        save = true,
+        filename = "spritesheet.png",
+        filetypes = { "png" },
+        entry = true
+    }:button { id = "confirm", text = "Confirm", onclick = function()
+        stop_script = false
+        dlg:close()
+    end
+    }:button { id = "cancel", text = "Cancel", onclick = function()
+        dlg:close()
+    end
+    }:show {
+        wait = true,
+    }.data["file_path"]
+
+    if stop_script then
+        return nil
+    end
+
+    return file_path
+end
+
+function openExportSpritesheetDialog()
+    os.remove(TMP_FILE_PATH)
+
+    local res = app.command.ExportSpriteSheet({
+        textureFilename = TMP_FILE_PATH
+    })
+
+    return app.fs.isFile(TMP_FILE_PATH)
+end
+
+function create_indexed_image(file_path)
+    -- Stolen from here https://github.com/mikeemm/aseprite-index-exporter/tree/master
+    local currentSprite = app.open(TMP_FILE_PATH)
+
+    local exportImageSpecs = currentSprite.spec
+    exportImageSpecs.colorMode = ColorMode.GRAY
+    local exportImage = Image(exportImageSpecs)
+    local sourceImage = Image(currentSprite.spec)
+    sourceImage:drawSprite(currentSprite, 1)
+    local pc = app.pixelColor
+    for it in sourceImage:pixels() do
+        local index = it()
+        exportImage:drawPixel(it.x, it.y, pc.graya(it()))
+    end
+
+    exportImage:saveAs(file_path)
+    app.alert(string.format("Spritesheet exported to '%s'.", file_path))
+    app.command.CloseFile()
+end
+
+main()
